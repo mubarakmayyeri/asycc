3b9536405c036029aa1b02e57d2f1e9849a4941a
Mubarak Mayyeri <mubarakmayyeri@gmail.com>
2023-04-11 20:53:57 +0530

    created crud operations

--- database.py
+++ database.py
@@ -8,5 +8,5 @@ from sqlalchemy.ext.declarative import declarative_base
 DB_URL = "sqlite:///todo.sqlite3"
 
 engine = create_engine(DB_URL, connect_args={'check_same_thread': False})
-sessionlocal = sessionmaker(autocmmit=False, autoflush=False, bind=engine)
-Base = declarative_base
+sessionlocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI, Request, Depends, Form, status
 import models
-from database import engine, sessoinlocal
+from database import engine, sessionlocal
 from sqlalchemy.orm import Session
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 
 models.Base.metadata.create_all(bind=engine)
@@ -14,7 +15,7 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 def get_db():
-    db = sessoinlocal()
+    db = sessionlocal()
     try:
         yield db
     finally:
@@ -25,4 +26,31 @@ async def home(request: Request, db: Session = Depends(get_db)):
     todos = db.query(models.Todo).order_by(models.Todo.id.desc())
 
     return templates.TemplateResponse("index.html", {"request": request, "todos": todos})
+
+@app.post("/add")
+async def add(request: Request, task: str = Form(...), db: Session = Depends(get_db)):
+    todo = models.Todo(task=task)
+    db.add(todo)
+    db.commit()
+    return RedirectResponse(url=app.url_path_for("home"), status_code=status.HTTP_303_SEE_OTHER)
+
+@app.get("/edit/{todo_id}")
+async def add(request: Request, todo_id: int, db: Session = Depends(get_db)):
+    todo = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
+    return templates.TemplateResponse("edit.html", {"request": request, "todo": todo})
+
+@app.post("/edit/{todo_id}")
+async def add(request: Request, todo_id: int, task: str = Form(...), completed: bool = Form(False), db: Session = Depends(get_db)):
+    todo = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
+    todo.task = task
+    todo.completed = completed
+    db.commit()
+    return RedirectResponse(url=app.url_path_for("home"), status_code=status.HTTP_303_SEE_OTHER)
+
+@app.get("/delete/{todo_id}")
+async def add(request: Request, todo_id: int, db: Session = Depends(get_db)):
+    todo = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
+    db.delete(todo)
+    db.commit()
+    return RedirectResponse(url=app.url_path_for("home"), status_code=status.HTTP_303_SEE_OTHER)
 
